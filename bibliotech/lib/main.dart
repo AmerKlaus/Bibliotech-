9d891d46fa8fcb0dc5c7f6014d9933f2bca0e12a
Amer <amer1jawabra@gmail.com>
2024-04-30 19:37:28 -0400

    added book details functionality, reserve button functionality

--- bibliotech/lib/main.dart
+++ bibliotech/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/gestures.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:math';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,8 +21,76 @@ class MyApp extends StatelessWidget {
       title: 'Bibliotech',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginPage(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    LoginPage(), // Your login page
+    BookListPage(), // Your BookList page
+    LibraryPage(), // Your Library page
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your App Title'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: MyNavigationBar(
+        currentIndex: _currentIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class MyNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onItemTapped;
+
+  MyNavigationBar({
+    required this.currentIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onItemTapped,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.login),
+          label: 'Login',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          label: 'Book List',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.library_books),
+          label: 'Library',
+        ),
+      ],
     );
   }
 }
@@ -34,12 +103,14 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   bool _obscurePassword = true;
 
   bool _isStrongPassword(String password) {
     // Password must be at least 8 characters long and contain a mix of letters, numbers, and symbols
-    String pattern = r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+{}|:"<>?~]).{8,}$';
+    String pattern =
+        r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+{}|:"<>?~]).{8,}$';
     RegExp regExp = RegExp(pattern);
     return regExp.hasMatch(password);
   }
@@ -75,7 +146,8 @@ class _RegisterPageState extends State<RegisterPage> {
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Error'),
-          content: Text('Password is not strong enough. It must be at least 8 characters long and contain a mix of letters, numbers, symbols, and at least one big letter.'),
+          content: Text(
+              'Password is not strong enough. It must be at least 8 characters long and contain a mix of letters, numbers, symbols, and at least one big letter.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
@@ -106,13 +178,17 @@ class _RegisterPageState extends State<RegisterPage> {
     }
 
     try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
 
       // Store additional user information in Firestore
-      await FirebaseFirestore.instance.collection('Users').doc(userCredential.user!.uid).set({
+      await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userCredential.user!.uid)
+          .set({
         'userId': userCredential.user!.uid, // Store user ID in Firestore
         'email': _emailController.text,
       });
@@ -180,7 +256,9 @@ class _RegisterPageState extends State<RegisterPage> {
                 decoration: InputDecoration(
                   labelText: 'Password',
                   suffixIcon: IconButton(
-                    icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
+                    icon: Icon(_obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility),
                     onPressed: () {
                       setState(() {
                         _obscurePassword = !_obscurePassword;
@@ -196,7 +274,9 @@ class _RegisterPageState extends State<RegisterPage> {
                 decoration: InputDecoration(
                   labelText: 'Confirm Password',
                   suffixIcon: IconButton(
-                    icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
+                    icon: Icon(_obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility),
                     onPressed: () {
                       setState(() {
                         _obscurePassword = !_obscurePassword;
@@ -219,32 +299,110 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 }
 
+class Book {
+  final String id;
+  final String title;
+  final String author;
+  final String publishedDate;
+  final String publisher;
+  final String description;
+  final double price;
+  final String imageURL;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.publishedDate,
+    required this.publisher,
+    required this.description,
+    required this.price,
+    required this.imageURL,
+  });
+}
+
 class BookListPage extends StatefulWidget {
   @override
   _BookListPageState createState() => _BookListPageState();
 }
 
 class _BookListPageState extends State<BookListPage> {
-  Future<List<dynamic>> fetchRandomBooks() async {
+  late Future<List<Book>> futureBooks = Future.value([]);
+  final CollectionReference reservedBooksCollection =
+      FirebaseFirestore.instance.collection('ReservedBooks');
+
+  Random random = Random();
+
+  Future<List<Book>> fetchRandomBooks() async {
     final response = await http.get(
       Uri.https('www.googleapis.com', '/books/v1/volumes', {
         'q': 'subject:fiction',
         'maxResults': '20',
         'key': 'AIzaSyB_Keln8dwtYVbQ9216wJxD4aqc3sXD514',
+        // Replace 'YOUR_API_KEY' with your actual API key
       }),
     );
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body)['items'];
+      final data = jsonDecode(response.body);
+      List<Book> books = [];
+
+      for (var item in data['items']) {
+        var volumeInfo = item['volumeInfo'];
+        var saleInfo = item['saleInfo'];
+
+        // Generate random price between $5 and $50
+        double price = 5 + random.nextDouble() * (50 - 5);
+
+        books.add(Book(
+          id: item['id'],
+          title: volumeInfo['title'] ?? 'Unknown Title',
+          author:
+              volumeInfo['authors'] != null && volumeInfo['authors'].isNotEmpty
+                  ? volumeInfo['authors'][0]
+                  : 'Unknown Author',
+          publishedDate: volumeInfo['publishedDate'] ?? 'Unknown Date',
+          publisher: volumeInfo['publisher'] ?? 'Unknown Publisher',
+          description: volumeInfo['description'] ?? 'No description available',
+          price: price,
+          imageURL: volumeInfo['imageLinks'] != null
+              ? volumeInfo['imageLinks']['thumbnail'] ?? ''
+              : '',
+        ));
+      }
+
+      return books;
     } else {
       throw Exception('Failed to fetch books');
+    }
+  }
+
+  // Function to reserve a book
+  Future<void> reserveBook(Book book) async {
+    try {
+      await reservedBooksCollection.add({
+        'userId': FirebaseAuth.instance.currentUser!.uid,
+        'bookId': book.id,
+        'title': book.title,
+        'author': book.author,
+        'publishedDate': book.publishedDate,
+        'publisher': book.publisher,
+        'description': book.description,
+        'price': book.price,
+        'imageURL': book.imageURL,
+        'reservedAt': Timestamp.now(),
+      });
+      // Show success message or perform any other action
+    } catch (e) {
+      print('Error reserving book: $e');
+      // Show error message or handle error as needed
     }
   }
 
   @override
   void initState() {
     super.initState();
-    fetchRandomBooks(); // Fetch books when the page is initialized
+    futureBooks = fetchRandomBooks();
   }
 
   @override
@@ -253,32 +411,186 @@ class _BookListPageState extends State<BookListPage> {
       appBar: AppBar(
         title: Text('Book List'),
       ),
-      body: FutureBuilder<List<dynamic>>(
-        future: fetchRandomBooks(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            List<dynamic> books = snapshot.data ?? [];
-            return ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                var book = books[index];
-                return ListTile(
-                  title: Text(book['volumeInfo']['title'] ?? 'No Title'),
-                  subtitle: Text(book['volumeInfo']['authors']?.join(', ') ?? 'No Authors'),
-                  leading: book['volumeInfo']['imageLinks'] != null
-                      ? Image.network(book['volumeInfo']['imageLinks']['thumbnail'])
-                      : Icon(Icons.book),
-                  onTap: () {
-                    // Handle book tap
-                  },
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder<List<Book>>(
+          future: futureBooks,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  Book book = snapshot.data![index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      leading: Image.network(book.imageURL),
+                      title: Text(book.title),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Author: ${book.author}'),
+                          SizedBox(height: 4.0),
+                          Text('Price: \$${book.price.toStringAsFixed(2)}'),
+                        ],
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () => reserveBook(book),
+                        // Call reserveBook function
+                        child: Text('Reserve'),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookDetailsPage(book: book),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class BookDetailsPage extends StatelessWidget {
+  final Book book;
+
+  BookDetailsPage({required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              book.imageURL,
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              book.title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text('Author: ${book.author}'),
+            Text('Published Date: ${book.publishedDate}'),
+            Text('Publisher: ${book.publisher}'),
+            SizedBox(height: 16.0),
+            Text('Description:'),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(book.description),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LibraryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Get the current user ID
+    String? userId = FirebaseAuth.instance.currentUser?.uid;
+
+    if (userId == null) {
+      // User is not logged in, display a message to log in
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Library'),
+        ),
+        body: Center(
+          child: Text('Please log in to view your reserved books.'),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Library'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('ReservedBooks')
+            .where('userId', isEqualTo: userId)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
             );
           }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // If there are no reserved books
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text('No books reserved yet.'),
+            );
+          }
+
+          // If there are reserved books, display them in a ListView
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+
+              // Check for null values before assigning to variables
+              String id = data['id'] ?? '';
+              String title = data['title'] ?? 'Unknown Title';
+              String author = data['author'] ?? 'Unknown Author';
+              String publishedDate = data['publishedDate'] ?? 'Unknown Date';
+              String publisher = data['publisher'] ?? 'Unknown Publisher';
+              String description = data['description'] ?? 'No description available';
+              double price = (data['price'] ?? 0.0).toDouble();
+              String imageURL = data['imageURL'] ?? '';
+
+              // Create a Book object from the data
+              Book reservedBook = Book(
+                id: id,
+                title: title,
+                author: author,
+                publishedDate: publishedDate,
+                publisher: publisher,
+                description: description,
+                price: price,
+                imageURL: imageURL,
+              );
+
+              // Display the reserved book in a ListTile or a custom widget
+              return ListTile(
+                leading: Image.network(reservedBook.imageURL),
+                title: Text(reservedBook.title),
+                subtitle: Text('Author: ${reservedBook.author}'),
+                // You can add more details here if needed
+              );
+            }).toList(),
+          );
         },
       ),
     );
@@ -296,7 +608,8 @@ class LoginPage extends StatelessWidget {
 
   Future<void> _loginWithEmailAndPassword(BuildContext context) async {
     try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
@@ -304,7 +617,7 @@ class LoginPage extends StatelessWidget {
       // Navigate to home screen after successful login
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => BookListPage()),
+        MaterialPageRoute(builder: (context) => MyHomePage()),
       );
     } catch (e) {
       // Handle errors
@@ -314,7 +627,8 @@ class LoginPage extends StatelessWidget {
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Error'),
-          content: Text('Failed to login. Please check your credentials and try again.'),
+          content: Text(
+              'Failed to login. Please check your credentials and try again.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
@@ -375,7 +689,8 @@ class LoginPage extends StatelessWidget {
                       ..onTap = () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => RegisterPage()),
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()),
                         );
                       },
                   ),
@@ -400,7 +715,8 @@ class LoginPage extends StatelessWidget {
                         // Navigate to forgot password page
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+                          MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage()),
                         );
                       },
                   ),
@@ -445,7 +761,8 @@ class ForgotPasswordPage extends StatelessWidget {
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Error'),
-          content: Text('Failed to send password reset email. Please try again.'),
+          content:
+              Text('Failed to send password reset email. Please try again.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
